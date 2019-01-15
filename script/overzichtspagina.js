7b90f40f720eccdc3b9b20023bc96ca6d660b8c0
ZairaGr <zaira.groenendal@gmail.com>
2019-01-15 17:20:54 +0100

    tweaken

--- script/overzichtspagina.js
+++ script/overzichtspagina.js
@@ -5,9 +5,7 @@
 ///*eslint 'no-console': 0*/
 
 //like en unlike een verhaal
-var likebutton;
-
-likebutton = document.querySelectorAll('.like');
+var likebutton = document.querySelectorAll('.like');
 
 for (var i = 0; i < likebutton.length; i++) {
     likebutton[i].addEventListener("click", function changeLikebutton() {
@@ -21,19 +19,25 @@ var downloadIcoon = document.querySelectorAll('.download');
 
 var popUp = document.querySelector('.popup');
 
-function toggleDownload() {
-    popUp.classList.toggle('popup-clicked');
-}
+var downloadNotieficatie = document.querySelector('header nav ul li:nth-of-type(2) a p'); //var pakken van DOM
+
+var downloadNummer = parseInt(downloadNotieficatie.innerText); //text in de p omzetten naar een nummer
 
 
 for (i = 0; i < downloadIcoon.length; i++) {
-    downloadIcoon[i].addEventListener("click", function toggleDownload() {
-        this.classList.toggle('downloaded');
+    downloadIcoon[i].addEventListener("click", function () {
+        this.classList.toggle('downloaded'); //button krijgt vinkjke
+        popUp.classList.toggle('popup-clicked'); //even popup tonen na de klik op button
+
+        if (this.classList.contains('downloaded')) {
+            downloadNummer++; //voeg een nummer toe aan de p
+            downloadNotieficatie.textContent = downloadNummer; // zet de nieuwe nummer in de p
+        } else {
+            downloadNummer = downloadNummer - 1; // trek een nummer van de p af
+            downloadNotieficatie.textContent = downloadNummer; // zet de nieuwe nummer in de p
+        }
+
     });
-}
-
-for (i = 0; i < downloadIcoon.length; i++) {
-    downloadIcoon[i].addEventListener('click', toggleDownload);
 }
 
 // Popup wordt weggehaald
@@ -41,9 +45,3 @@ function removePopup() {
     popUp.classList.remove('popup-clicked');
 }
 popUp.addEventListener('click', removePopup);
-
-
-
-//loading state
-
-
